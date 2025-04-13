75704b3837c6ff52c08ef0ef1a861b4cc285f025
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-04-14 00:52:41 +0700

    Use React-router-dom to manage url

--- client/src/Components/NavbarItem.jsx
+++ client/src/Components/NavbarItem.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const NavbarItem = ({url, icon, children}) => {
+const NavbarItem = ({ url, icon, children, className, onClick }) => {
   return (
     <a
-      href={`${url}`}
-      className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-100 rounded-lg"
+      href={url}
+      className={`flex items-center px-6 py-3 text-gray-600 hover:bg-gray-100 rounded-lg ${className}`}
+      onClick={onClick}
     >
-          <span className="mr-3">{icon}</span> {children}
+      <span className="mr-3">{icon}</span> {children}
     </a>
   );
-}
+};
 
-export default NavbarItem
+export default NavbarItem;
