c40ea4f952ca95d40aced30a8010c059dec7e185
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-04-14 00:51:31 +0700

    Add class click into navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import DBContent from "./Components/DBContent";
 import Button from "./Components/Button";
@@ -6,36 +7,23 @@ import Profile from "./Pages/Profile";
 
 function App() {
   return (
-    <div className="flex">
-      <Navbar />
-      <div className="flex-1">
-        <header className="bg-gray-900 text-white p-6 flex justify-between items-center">
-          <div className="flex flex-col">
-            <h1 className="text-xl">Dashboard/</h1>
-            <h1 className="text-xl font-bold">Dashboard</h1>
-          </div>
-          <div className="flex items-center space-x-4">
-            <div className="relative">
-              <input
-                className="bg-gray-800 text-white px-4 py-2 rounded-lg pl-10"
-                type="text"
-                placeholder="Search"
-              />
-              <span className="absolute left-3 top-2.5 text-white">🔍</span>
+    <Router>
+      <div className="flex">
+        <Navbar />
+        <div className="flex-1">
+          <header className="bg-gray-900 text-white p-6 flex justify-between items-center">
+            <div className="flex flex-col">
+              <h1 className="text-xl">Dashboard/</h1>
+              <h1 className="text-xl font-bold">Dashboard</h1>
             </div>
-            <Button icon="fas fa-user" color="blue" text="Sign In" />
-            <select className="bg-gray-800 text-white px-4 py-2 rounded-lg">
-              <option value="1">trannghiach2</option>
-              <option value="2">Notification 2</option>
-              <option value="3">Notification 3</option>
-            </select>
-            <Button icon="fas fa-setting" color="gray" text="Settings" />
-          </div>
-        </header>
-        <DBContent />
-        <Profile /> 
+          </header>
+          <Routes>
+            <Route path="/" element={<DBContent />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,32 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState("/");
+
+  const handleItemClick = (url) => {
+    setActiveItem(url);
+  };
+
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
       <div className="w-80 bg-white shadow-md">
         <div className="p-5 bg-gray-900 text-white flex items-center">
           <a href="/">
-            <h1 className="text-2xl font-bold">
-              EMPLOYEES MANAGEMENT
-            </h1>
+            <h1 className="text-2xl font-bold">EMPLOYEES MANAGEMENT</h1>
           </a>
         </div>
         <nav className="mt-6">
           <ul className="text-xl">
             <li className="mb-2">
-              <NavbarItem url="/" icon ="🏠">Dashboard</NavbarItem>
+              <NavbarItem
+                url="/"
+                icon="🏠"
+                className={`${activeItem === "/" ? "bg-black text-white" : ""}`}
+                onClick={() => handleItemClick("/")}
+              >
+                Dashboard
+              </NavbarItem>
             </li>
             <li className="mb-2">
-              <NavbarItem url="/employees" icon ="👤">Profile</NavbarItem>
+              <NavbarItem
+                url="/employees"
+                icon="👤"
+                className={`${
+                  activeItem === "/employees" ? "bg-black text-white" : ""
+                }`}
+                onClick={() => handleItemClick("/employees")}
+              >
+                Profile
+              </NavbarItem>
             </li>
             <li className="mb-2">
-              <NavbarItem url="/users" icon ="📊">Users</NavbarItem>
+              <NavbarItem
+                url="/users"
+                icon="📊"
+                className={`${
+                  activeItem === "/users" ? "bg-black text-white" : ""
+                }`}
+                onClick={() => handleItemClick("/users")}
+              >
+                Users
+              </NavbarItem>
             </li>
             <li className="mb-2">
-              <NavbarItem url="/notifications" icon ="🔔">Notifications</NavbarItem>
+              <NavbarItem
+                url="/notifications"
+                icon="🔔"
+                className={`${
+                  activeItem === "/notifications" ? "bg-black text-white" : ""
+                }`}
+                onClick={() => handleItemClick("/notifications")}
+              >
+                Notifications
+              </NavbarItem>
             </li>
           </ul>
         </nav>
@@ -34,10 +71,28 @@ const Navbar = () => {
           <p className="text-sm font-semibold text-gray-500">AUTH PAGES</p>
           <ul className="mt-4 text-xl">
             <li className="mb-2">
-              <NavbarItem url="/login" icon ="🔑">Sign In</NavbarItem>
+              <NavbarItem
+                url="/login"
+                icon="🔑"
+                className={`${
+                  activeItem === "/login" ? "bg-black text-white" : ""
+                }`}
+                onClick={() => handleItemClick("/login")}
+              >
+                Sign In
+              </NavbarItem>
             </li>
             <li className="mb-2">
-              <NavbarItem url="/register" icon ="📝">Sign Up</NavbarItem>
+              <NavbarItem
+                url="/register"
+                icon="📝"
+                className={`${
+                  activeItem == "/register" ? "bg-black text-white" : ""
+                }`}
+                onClick={() => handleItemClick("/register")}
+              >
+                Sign Up
+              </NavbarItem>
             </li>
           </ul>
         </div>
